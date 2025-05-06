16a54ed10b367f4a8b842d03de95f3953c72bb4b
Anshikapal05 <anshikapal546@gmail.com>
2025-05-06 11:33:18 +0530

    chage in server

--- server.js
+++ server.js
@@ -65,9 +65,11 @@ const allowedOrigins = [
         callback(new Error('Not allowed by CORS'));
       }
     },
+    credentials: true
   };
   
   app.use(cors(corsOptions));
+  app.options('*', cors(corsOptions));
 //api endpoints
 
 app.use('/api/admin',adminRouter)
